b523e56cf9d54788ce0ac1e465ed33d3c7dd7d0f
Ryan Stephens <ryan_stephens99@hotmail.com>
2016-11-26 14:51:25 -0500

    Added animation css

--- js/annoy.js
+++ js/annoy.js
@@ -1,19 +1,36 @@
-var TIME_COUNT = 0
-var onetimePrompt = 10;
-var outboundPrompt = 15;
+var TIME_COUNT = 0;
+var onetimePromptTime = 10;
+var outboundPromptTime = 15;
+var randomClickTime = 30;
+var hingeTime = 0;
+
 
 function doUpdate() {
 	
-	// One-time prompt at 15s
-	if (TIME_COUNT === 1000 * onetimePrompt) {
+	// One-time prompt at 10s
+	if (TIME_COUNT === 1000 * onetimePromptTime) {
 		alert("You should probably stop procrastinating :^)");
 	}
 }
 
-$("a").click(function () {
-	if (TIME_COUNT >= outboundPrompt * 1000) {
+function randomLinkClick() {
+   var links = $('a');
+   var randomNum = Math.floor(Math.random()*links.length)  
+   links.get(randomNum).click();
+} 
+
+$("a").mouseover(function() {
+	$("a").addClass("animate.animated.hinge");
+});
+
+$("a").click(function () {	
+	if (TIME_COUNT >= outboundPromptTime * 1000) {
 		var response = confirm("Press Cancel");
 		if (response) {
+			return false;
+		} else {
+			if (TIME_COUNT >= randomClickTime * 1000) 
+				randomLinkClick();
 			return false;
 		}
 	}
@@ -21,19 +38,34 @@ $("a").click(function () {
 
 
 $("img").click(function () {
-	if (TIME_COUNT >= outboundPrompt * 1000) {
+	if (TIME_COUNT >= outboundPromptTime * 1000) {
 		var response = confirm("Press Cancel");
 		if (response) {
 			return false;
-		} 
+		} else {
+			
+		}
 	}
 });
+
+function debugOut() {
+	if (TIME_COUNT === onetimePromptTime * 1000) {
+		console.log("Alert dialogue should be prompted NOW");
+	}
+	if (TIME_COUNT === outboundPromptTime * 1000) {
+		console.log("Yes/Cancel dialogue should be enabled NOW");
+	}
+	if (TIME_COUNT === randomClickTime * 1000) {
+		console.log("Cancelling dialogue should redirect to random link NOW");
+	}
+}
 
 
 // 1 second timer
 setInterval( function () {
 	TIME_COUNT += 1000;
 	doUpdate();
+	debugOut();
 		
 }, 1000);
 
